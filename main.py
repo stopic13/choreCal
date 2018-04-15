8749e02a7f8a04ffbddc93543c7c697af2ec9a48
stopic13 <sara.topic.18@dartmouth.edu>
2018-04-14 22:09:42 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+
+dict = {}
+import icalendar
+
+# input: my master calendar
+filepath = "Chores 17S.ics"
+g = open(filepath,'rb')
+gcal = icalendar.Calendar.from_ical(g.read())
+i = 0
+
+name = ""
+# for each name
+for resident in residents:
+    print resident
+    # 	make a new calendar
+    cal = icalendar.Calendar()
+
+    #for each event
+    for component in gcal.walk('vevent'):
+        summary = component.get('summary')
+        if ":" in summary:
+
+            chore,name = summary.split(":")
+            chore.strip()
+            name.strip()
+
+            #if name in event name
+            if resident in name:
+
+                #add to calendar
+                event = icalendar.Event()
+                event['summary'] = "CHORE: " + str(chore)
+                event['dtstart'] = component.get('dtstart')
+
+                #print resident, name
+                if resident in dict:
+                    dict[resident] += 1
+                else:
+                    dict[resident] = 1
+                print resident, chore
+
+                cal.add_component(event)
+
+    filename = resident + ".ics"
+    f = open(filename, 'wb')
+
+    f.write(cal.to_ical())
+    f.close()
+
+g.close()
+for key in dict:
+    print  dict[key], key
